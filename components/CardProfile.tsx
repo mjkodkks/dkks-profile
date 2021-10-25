72af2498472dc89d2c1409eba9c18b51e01cdbec
DKKs <eye.q.21@gmail.com>
2021-10-26 02:29:43 +0700

    [Add] card profile

--- components/CardProfile.tsx
+++ components/CardProfile.tsx
@@ -1,26 +1,68 @@
-import Image from "next/image";
-import { useEffect, useState } from "react";
+/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image'
+import { useEffect, useRef, useState } from "react";
+import { default as anime } from '../node_modules/animejs/lib/anime.es.js';
 
-const CardProfile = ()=> {
+const CardProfile = () => {
     const [img, setImg] = useState("");
+    let cardProfileRef = useRef(null);
 
-    const getImage = async ()=> {
-        const res = await fetch(`https://api.github.com/users/mjkodkks`) 
+    const getImage = async () => {
+        const res = await fetch(`https://api.github.com/users/mjkodkks`)
         const data = await res.json()
         setImg(data.avatar_url)
+        return res
     }
+
+    const animate = ()=> {
+        console.log(cardProfileRef.current)
+        // anime({
+        //     targets: '.card-profile',
+        //     translateX: 250,
+        //     translateY: 200
+        //   });
+
+        var tl = anime.timeline({
+        easing: 'easeOutExpo',
+        duration: 1000,
+        loop: true,
+        });
+
+        tl.add({
+            targets: '.card-profile',
+            keyframes: [
+                {translateY: -40},
+                {translateY: 0}
+              ],
+        })
+    }
+
     // Similar to componentDidMount and componentDidUpdate:
     useEffect(() => {
         getImage()
-    },[]);
+        animate()
+    }, []);
 
     return (
         <>
-          <div className="rounded-lg border h-6 p-8">
-              <div className="hi">
-                  <img src={img} alt="" />
-              </div>
-          </div>
+            <div className="card-profile flex gap-8 rounded-xl border max-w-2xl p-8 bg-white"
+            ref={cardProfileRef} >
+                <div className="img-warpper">
+                    <img src={img} alt="img_profile"
+                    className="rounded-full" 
+                    />
+                </div>
+                <div>
+                    <div className="text-lg">Hi i&apos;m <b>Thanonphat</b></div>
+                    <div className="mt-4">My name is Thanonphat Supho. I&apos;m Front-End Developer from thailand.
+                        I&apos;m a cat lover and enjoy to make something new.
+                    </div>
+                    <div className="flex gap-4 mt-4">
+                        <img className="w-8 cursor-pointer" src="/github.png" alt="github" />
+                        <img className="w-8 cursor-pointer" src="/linkedin.png" alt="linkedin" />
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
+  important: true,
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        profileWidth: 400
+      },
+      height: {
+        profileHeight: 250
+      },
+      color: {
+        primary: "#000",
+        background: "#101018"
+      }
+    },
   },
   variants: {
     extend: {},
